253fa8bf4ae12ca377df5ad42d31527ef22e8c0e
Igor Ferreira <igor-freitas10@hotmail.com>
2022-07-27 16:17:56 -0300

    Implemented Category class

--- budget.py
+++ budget.py
@@ -1,7 +1,36 @@
 class Category:
+    def __init__(self, name:str):
+        self.name = name
+        self.ledger = list()
+        self.balance = 0
 
+    def deposit(self, amount:float, description:str='') -> None:
+        self.balance += amount
+        self.ledger.append({'amount': amount, 'description': description})
 
+    def get_balance(self) -> float:
+        return self.balance
 
+    def check_funds(self, amount:float) -> bool:
+        return  amount <= self.get_balance()
 
+    def withdraw(self, amount:float, description:str='') -> bool:
+        if not self.check_funds(amount): return False 
+        
+        self.balance -= amount
+        self.ledger.append({'amount': -amount, 'description': description})        
+        return True
 
-def create_spend_chart(categories):
+    def transfer(self, amount:float, category:'Category') -> bool:
+        if not self.check_funds(amount): return False
+        
+        self.withdraw(amount, f'Transfer to {category.name}')
+        category.deposit(amount, f'Transfer from {self.name}')
+        return True
+    
+    def __str__(self) -> str:
+        item_display = '{description:<23.23s}{amount:>7.2f}' 
+        transactions = [item_display.format(**transaction) for transaction in self.ledger]
+        return f'{self.name:*^30}\n' + '\n'.join(transactions) + f'\nTotal: {self.get_balance():.2f}'
+
+# def create_spend_chart(categories):
