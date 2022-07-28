77b3e9d1d81dee5b6b0e7d2c9b93a43d50ee553d
Igor Ferreira <igor-freitas10@hotmail.com>
2022-07-28 18:09:00 -0300

    Add create_spend_chart function

--- budget.py
+++ budget.py
@@ -1,3 +1,5 @@
+from itertools import zip_longest
+
 class Category:
     def __init__(self, name:str):
         self.name = name
@@ -33,4 +35,37 @@ class Category:
         transactions = [item_display.format(**transaction) for transaction in self.ledger]
         return f'{self.name:*^30}\n' + '\n'.join(transactions) + f'\nTotal: {self.get_balance():.2f}'
 
-# def create_spend_chart(categories):
+
+def create_spend_chart(categories:list):    
+    title = 'Percentage spent by category'
+    row = '\n{:3}{sep}' + ('{:^3}' * len(categories)) + ' '
+    dashed_sep = f'{"":4}{"":->{(3 * len(categories) + 1)}}'
+
+    x_labels, y_labels, expenses = [], range(100, -1, -10), []
+
+    for category in categories:
+        x_labels.append(getattr(category, 'name'))
+        expenses.append(get_total_spend_by_category(category))
+    
+    percentages = calc_percent_of_expenses(expenses)
+    bars = generate_bars(percentages)
+    
+    chart = title
+    for data in zip_longest(y_labels, *bars): chart += row.format(*data, sep='|')
+    chart += f'\n{dashed_sep}'
+    for letters in zip_longest(*x_labels, fillvalue=' '): chart += row.format('', *letters, sep=' ')
+
+    return chart
+
+
+def get_total_spend_by_category(category:Category):
+    return sum(transaction['amount'] for transaction in getattr(category, 'ledger') if transaction['amount'] < 0)
+
+
+def calc_percent_of_expenses(expenses:list):
+    total_spend = sum(expenses)
+    return tuple([int(expense / total_spend * 10) for expense in expenses])
+
+
+def generate_bars(percents:list):  
+    return tuple((10 - percent) * ' ' + 'o' * (percent + 1) for percent in percents)
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
-# from budget import create_spend_chart
+from budget import create_spend_chart
 from unittest import main
 
+
 food = budget.Category("Food")
+clothing = budget.Category("Clothing")
+auto = budget.Category("Auto")
+
 food.deposit(1000, "initial deposit")
 food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = budget.Category("Clothing")
+food.withdraw(15.89, "restaursant and more food for dessert")
 food.transfer(50, clothing)
+
 clothing.withdraw(25.55)
 clothing.withdraw(100)
-auto = budget.Category("Auto")
+
 auto.deposit(1000, "initial deposit")
 auto.withdraw(15)
 
-print(food)
-print(clothing)
+for category in [food, clothing, auto]:
+    print(category, end='\n\n')
 
-# print(create_spend_chart([food, clothing, auto]))
+print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
